8b1fedc4d25ee6c5794146a69217ffa04aea214c
Guzina Jasmin <guzinaja@promelit.lan>
2021-11-04 15:01:22 +0100

    Values and Variables

---
+++ 02_values_and_variables.js
@@ -0,0 +1,13 @@
+// ***************  NOTES: VALUES AND VARIABLES  ************************
+
+// 1- Variables name should not start with a number let 3name = "Jasmin" is going to through a syntax error
+
+// 2- Variable names can contain letters, numbers, underscores or $ (dollar) sign if I try to declare a variable name first&name is going to through an error on the console
+
+// 3- Variable names should not be same to reserved language words for example <i>new</i> or <i>function</i>.
+
+// 4- Variable names should not start with uppercase letter let Person, usually this syntax is being used for classes in JS
+
+// 5- Constants should be written in uppercase, so it is easy to understand that the value store is not going to change. let PI = 3.14
+
+// 6- Variables name should be descriptive
