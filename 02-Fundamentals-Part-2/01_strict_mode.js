8bfa47c820f5976ccab14a8a6448400c0a550b43
Guzina Jasmin <guzinaja@promelit.lan>
2021-11-08 10:43:31 +0100

    Usage and advantages of using Strict mode

---
+++ 02-Fundamentals-Part-2/01_strict_mode.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// Enable strict mode to check the problem on the line above
+// if (passTest) hasDriverLicense = true;
+if (passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log("I can drive :D");
