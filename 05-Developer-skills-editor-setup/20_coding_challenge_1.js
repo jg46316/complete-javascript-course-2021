0183de20d85265e33aea5e8a4301ec4c0858d4d1
Guzina Jasmin <guzinaja@promelit.lan>
2021-11-10 15:03:27 +0100

    small fix

--- 05-Developer-skills-editor-setup/20_coding_challenge_1.js
+++ 05-Developer-skills-editor-setup/20_coding_challenge_1.js
@@ -19,10 +19,10 @@ const data2 = [12, 5, -5, 0, 4];
 const printForecast = function (arr) {
   let str = "";
   for (let i = 0; i < arr.length; i++) {
-    str = str + `${arr[i]}ºC in ${i + 1} days ... `;
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
   }
 
-  console.log(str);
+  console.log("..." + str);
 };
 
 printForecast(data1);
