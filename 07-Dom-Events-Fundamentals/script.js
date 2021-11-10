04ed686092980cd02999bf23bef69de3d1fe0229
Guzina Jasmin <guzinaja@promelit.lan>
2021-11-10 17:10:14 +0100

    Guess My Number! GAME

---
+++ 07-Dom-Events-Fundamentals/script.js
@@ -0,0 +1,43 @@
+/*
+What is DOM?
+DOCUMENT OBJECT MODEL: Structured representation of HTML documents. Allows Javascript to access HTML elements and Styles to Manipulate them.
+DOM and properties for DOM MANIPULATION  not part of JS (ECMA) for example document.querySelector()
+WEB APIs DOM Methods and Properties (Timers, Fetch) can interact with JS
+*/
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+
+const x = function () {
+    console.log(23);
+};
+*/
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
