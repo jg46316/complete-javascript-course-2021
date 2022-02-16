c8ab7ac425b3c2b332ee032271eda6b762341b0e
Jasmin <jasmin.guzina@studioartlan.com>
2022-02-16 14:52:47 +0100

    Value vs References

--- 10-A-Closer-Look-At-Functions/passing_value_vs_reference.js
+++ 10-A-Closer-Look-At-Functions/passing_value_vs_reference.js
@@ -3,22 +3,32 @@
 const flight = 'LH234';
 
 const jasmin = {
-    name: 'Jasmin Guzina',
-    passport: 24554544545
-}
+  name: 'Jasmin Guzina',
+  passport: 24554544545,
+};
 
-const checkIn = function(flightNum, passenger) {
-flightNum = 'LH999';
-passenger.name = 'Mr.' + passenger.name;
-if(passenger.passport === 24554544545){
-    alert('Checked in')
-}else{
-    alert('Wrong passport')
-}
-
-}
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+  if (passenger.passport === 24554544545) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
 
 checkIn(flight, jasmin);
 
 console.log(flight);
 console.log(jasmin);
+
+// Is the same as doing ...
+const flightNum = flight;
+const passenger = jasmin;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(jasmin);
+checkIn(flight, jasmin);
